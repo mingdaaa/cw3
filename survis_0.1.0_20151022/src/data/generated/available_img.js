d1184e4efc773601b8aee03f898b718e8cb37a75
mingdaaa <117132846+mingdaaa@users.noreply.github.com>
2023-05-14 12:59:48 +0100

    Add files via upload

---
+++ survis_0.1.0_20151022/src/data/generated/available_img.js
@@ -0,0 +1,1 @@
+define({ availableImg: ["A2013Information","A2020Computers","An2023Information","Assessing2021Journal","Automated2021Expert","Braekers2016Computers","Classical2000International","The2020IEEE","Vehicle2010Journal","Vehicle2012In"]});
